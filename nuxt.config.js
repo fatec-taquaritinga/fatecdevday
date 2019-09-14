7bfc1c30c000042fcbc9fb16bbd70cb999be727d
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2019-09-14 18:01:58 -0300

    Configurated PWA for social medias embedding

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,30 @@
+const data = {
+  lang: 'pt-br',
+  name: 'Fatec Dev Day',
+  title: 'Fatec Dev Day 2019',
+  description: 'Fatec Dev Day: um dia dedicado a se atualizar sobre desenvolvimento Web e Mobile, linguagens de programação e sistemas em geral',
+  domain: 'https://fatecdevday.com.br/',
+  thumbnail: 'https://fatecdevday.com.br/thumbnail.jpg'
+}
+
 export default {
   head: {
-    title: 'Fatec Dev Day 2019',
+    title: data.title,
     htmlAttrs: {
-      lang: 'pt-br'
+      lang: data.lang
     },
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  meta: {
-    lang: 'pt-br',
-    name: 'Fatec Dev Day 2019',
-    description: 'Fatec Dev Day: um dia dedicado a desenvolvimento Web, aplicativos e sistemas em geral'
-  },
+  meta: [
+    { lang: data.lang },
+    { charset: 'utf-8' },
+    { name: data.title },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: data.description },
+    { hid: 'thumbnail', name: 'thumbnail', content: data.domain + data.thumbnail }
+  ],
   loading: { color: '#ad0f0a' },
   generate: {
     fallback: true
@@ -24,11 +36,17 @@ export default {
     '@nuxtjs/pwa'
   ],
   manifest: {
-    name: 'Fatec Dev Day 2019',
-    short_name: 'Fatec Dev Day',
-    lang: 'pt-br',
+    name: data.title,
+    short_name: data.name,
+    lang: data.lang,
     start_url: '/',
-    description: 'Fatec Dev Day: um dia dedicado a desenvolvimento Web, aplicativos e sistemas em geral'
+    description: data.description,
+    ogSiteName: data.name,
+    ogTitle: data.title,
+    ogDescription: data.description,
+    ogHost: data.domain,
+    ogImage: data.thumbnail,
+    twitterCard: 'summary_large_image'
   },
   build: {
     extend (config, { isDev, isClient }) {
